09aed2704ea9c6eab32f23f77aa0d69024a45fbb
Manish <mkk725050@gmail.com>
2026-02-09 01:59:00 +0530

    Container & Basic Layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Container Demo Home Page'),
+      home: const MyHomePage(title: 'Container & Basic Layout'),
     );
   }
 }
